4519464733df397879938c3d0e0b48cf1a921c1c
MichalStuchlik <D21632@oaopava.cz>
2024-10-31 09:31:51 +0100

    reseni

--- pole_mesic.php
+++ pole_mesic.php
@@ -8,5 +8,20 @@
 
 //reseni
 
+$mesice = [
+    1 => "Leden", 2 => "Únor", 3 => "Březen", 4 => "Duben", 
+    5 => "Květen", 6 => "Červen", 7 => "Červenec", 8 => "Srpen", 
+    9 => "Září", 10 => "Říjen", 11 => "Listopad", 12 => "Prosinec"
+];
+
+foreach ($mesice as $value) {
+    echo $value .'<br />';
+}
+
+for ($i = 6; $i <= 12; $i++) {
+    echo $mesice[$i] . "<br>";
+}
+
+
 ?>
 
